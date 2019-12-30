79ee58749fdb0ea550a23ffb1a40a18105cef48e
Tomi Tarvainen <tomi.tarvainen@alten.fi>
2019-12-30 08:29:43 +0200

    Add title text to home page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
+import {
+  makeStyles,
+  Drawer,
+  List,
+  Divider,
+  ListItem,
+  ListItemText,
+  Container
+} from '@material-ui/core';
 
 const drawerWidth = 240;
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: 'flex',
-  },
-  appBar: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth,
-  },
+const useStyles = makeStyles({
   drawer: {
     width: drawerWidth,
     flexShrink: 0,
@@ -24,43 +19,55 @@ const useStyles = makeStyles(theme => ({
   drawerPaper: {
     width: drawerWidth,
   },
-  content: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.default,
-    padding: theme.spacing(3),
-  },
-}));
+  myTitle: {
+    position: "absolute",
+    top: "height / 50%",
+    left: "50%",
+    bottom: "50%",
+    color: "red",
+    backgroundColor: "#0ff",
+    boxShadow: "0 0 40px 20px #0ff"
+  }
+});
 
 export default function PermanentDrawerLeft() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <Drawer
-        className={classes.drawer}
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-        anchor="left"
-      >
-        <div className={classes.toolbar} />
-        <Divider />
-        <List>
-          {['Home', 'About', 'Skills', 'Resume', 'Contact'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
+    <Container >
 
-      </Drawer>
-      <main className={classes.content}>
-        <div className={classes.toolbar} />
-        <img src="https://images.unsplash.com/photo-1550645612-83f5d594b671?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" width="100%" alt="lovely" />
-      </main>
-    </div>
+      <Container>
+        <Drawer
+          className={classes.drawer}
+          variant="permanent"
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+          anchor="left"
+        >
+          <Container className={classes.toolbar} />
+          <Divider />
+          <List>
+            {['Home', 'About', 'Skills', 'Resume', 'Contact'].map((text) => (
+              <ListItem button key={text}>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
+      </Container>
+
+      <Container>
+        <h2 className={classes.myTitle}>
+          Hi, I'm Tomi, a developer.
+        </h2>
+        <Container>
+          <img
+            src="https://images.unsplash.com/photo-1550645612-83f5d594b671?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="a lot of code"
+          />
+        </Container>
+      </Container>
+
+    </Container>
   );
 }
